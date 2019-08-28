bd66533a4eb8ef8e0669194cc92a5652dc5b04d2
Maynard Hartman <mjhartman@brighthouse.com>
2019-08-28 07:53:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,17 +16,17 @@ a_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  p array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  p array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  p array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
